89f76bf2c5f0c5941cc0cb9bee9510488b0b486b
mazharul-islam321 <mazharulislam.lu2016@gmail.com>
2023-08-12 03:32:04 +0600

    some fix on login and register form

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -14,7 +14,7 @@ const Login = () => {
 	return (
 		<div className="hero min-h-screen bg-base-200">
 			<div className="hero-content flex-col">
-				<div className="text-center lg:text-left">
+				<div className="text-center ">
 					<h1 className="text-5xl font-bold mb-4">Please Login !</h1>
 				</div>
 				<div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
--- src/page/Register.tsx
+++ src/page/Register.tsx
@@ -1,14 +1,38 @@
 const Register = () => {
+	const handleRegister = (event: {
+		preventDefault: () => void;
+		target: any;
+	}) => {
+		event.preventDefault();
+
+		const form = event.target;
+		const name = form.name.value;
+		const email = form.email.value;
+		const pasword = form.password.value;
+		console.log("object->>", name, email, pasword);
+	};
 	return (
 		<div className="hero min-h-screen bg-base-200">
 			<div className="hero-content flex-col">
-				<div className="text-center lg:text-left">
+				<div className="text-center">
 					<h1 className="text-5xl font-bold mb-4">
 						Please Register !
 					</h1>
 				</div>
 				<div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-					<form className="card-body">
+					<form onSubmit={handleRegister} className="card-body">
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Name</span>
+							</label>
+							<input
+								type="text"
+								name="name"
+								placeholder="name"
+								className="input input-bordered"
+								required
+							/>
+						</div>
 						<div className="form-control">
 							<label className="label">
 								<span className="label-text">Email</span>
@@ -32,14 +56,6 @@ const Register = () => {
 								className="input input-bordered"
 								required
 							/>
-							<label className="label">
-								<a
-									href="#"
-									className="label-text-alt link link-hover"
-								>
-									Forgot password?
-								</a>
-							</label>
 						</div>
 						<div className="form-control mt-6">
 							<button className="btn btn-primary">Login</button>
